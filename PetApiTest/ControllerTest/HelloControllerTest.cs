271bada9a9381eebf4729592594570d60bbe2f5d
Guangguang Ma <ggma@thoughtworks.com>
2022-10-29 23:58:20 +0800

    init commit

---
+++ PetApiTest/ControllerTest/HelloControllerTest.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc.Testing;
+using Xunit;
+
+namespace PetApiTest.ControllerTest;
+
+public class HelloControllerTest
+{
+    [Fact]
+    public async void Should_return_hello_world()
+    {
+    }
+}
